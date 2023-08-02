7bd807fd1799c3315e47688842dd698fdfa39e20
Adam Montgomerie <adam.montgomerie971@gmail.com>
2023-08-02 15:49:05 +0100

    measure validation by epoch not step

--- package/commonlit_summaries/experiment.py
+++ package/commonlit_summaries/experiment.py
@@ -70,9 +70,7 @@ class Experiment:
             print(f"Epoch: {epoch} | {metrics}")
 
             if self.use_wandb:
-                wandb.log(
-                    {"eval_" + name: metric for name, metric in metrics.items()}, step=self.step
-                )
+                wandb.log({"eval_" + name: metric for name, metric in metrics.items()}, step=epoch)
 
             if self.save_strategy == "all":
                 self._save(self.fold, epoch)
