b6a263e829c9121b00f5bfddcd1e3e9e9ae92220
Adam Montgomerie <adam.montgomerie971@gmail.com>
2023-08-18 17:30:10 +0100

    Fix variable name clash

--- package/commonlit_summaries/validate.py
+++ package/commonlit_summaries/validate.py
@@ -11,15 +11,15 @@ app = typer.Typer(add_completion=False)
 @app.command()
 def main(oof_predictions_path: str = typer.Option(..., "--path")):
     predictions = pd.read_csv(oof_predictions_path)
-    fold_scores = []
+    scores_per_fold = []
 
     for fold in predictions.prompt_id.unique():
         fold_predictions = predictions.loc[predictions.prompt_id == fold]
         fold_scores = compute_metrics(fold_predictions)
         typer.echo(f"Fold {fold} | fold_scores")
-        fold_scores.append(list(fold_scores.values()))
+        scores_per_fold.append(list(fold_scores.values()))
 
-    cv_score = np.mean(fold_scores, axis=0)
+    cv_score = np.mean(scores_per_fold, axis=0)
     typer.echo(f"CV | MCRMSE {cv_score[0]} | Content {cv_score[1]} " f"| Wording {cv_score[2]}")
 
     oof_scores = compute_metrics(predictions)
