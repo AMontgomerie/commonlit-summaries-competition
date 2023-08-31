f9bcb6e02dec3183f71a37094a1a2d5a503df038
Adam Montgomerie <adam.montgomerie971@gmail.com>
2023-08-31 17:16:36 +0100

    Fix validation script

--- package/scripts/validate.py
+++ package/scripts/validate.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import typer
 
-from commonlit_summaries.metrics import compute_metrics
+from commonlit_summaries.metrics import compute_mcrmse
 
 
 app = typer.Typer(add_completion=False)
@@ -15,14 +15,14 @@ def main(oof_predictions_path: str = typer.Option(..., "--path")):
 
     for fold in predictions.prompt_id.unique():
         fold_predictions = predictions.loc[predictions.prompt_id == fold]
-        fold_scores = compute_metrics(fold_predictions)
+        fold_scores = compute_mcrmse(fold_predictions)
         typer.echo(f"Fold {fold} | {fold_scores}")
         scores_per_fold.append(list(fold_scores.values()))
 
     cv_score = np.mean(scores_per_fold, axis=0)
     typer.echo(f"CV | MCRMSE {cv_score[0]} | Content {cv_score[1]} " f"| Wording {cv_score[2]}")
 
-    oof_scores = compute_metrics(predictions)
+    oof_scores = compute_mcrmse(predictions)
     typer.echo(f"OOF | {oof_scores}")
 
 
